20fb06a49824ab449c78d7210d2dae92e8d67403
nilufar <nilufar@gmail.com>
2025-04-05 15:43:51 +0500

    My portfoilo

--- js/data.js
+++ js/data.js
@@ -1,37 +1,30 @@
 export default [
-    {
-        title: "Ansormed",
-        github: "https://github.com/nilushaa/ansormed1",
-        vercel: "https://ansormed1.vercel.app",  
-    },
+  {
+    title: "Ansormed",
+    github: "https://github.com/nilushaa/ansormed1",
+    vercel: "https://ansormed1.vercel.app",
+  },
 
-    {
-        title: "Ansormed",
-        github: "https://github.com/nilushaa/ansormed1",
-        vercel: "https://ansormed1.vercel.app",  
-    },
+  {
+    title: "akademnashiir",
+    github: "https://github.com/nilushaa/akademnashiir",
+    vercel: "https://akademnashiir.vercel.app",
+  },
 
-    {
-        title: "akademnashiir",
-        github: "https://github.com/nilushaa/akademnashiir",
-        vercel: "https://akademnashiir.vercel.app",  
-    },
+  {
+    title: "sport-power",
+    github: "https://github.com/nilushaa/sport-power",
+    vercel: "https://sport-power.vercel.app",
+  },
 
-    {
-        title: "sport-power",
-        github: "https://github.com/nilushaa/sport-power",
-        vercel: "https://sport-power.vercel.app",  
-    },
-
-    {
-        title: "nilushaa/3-oy-1-figmaa",
-        github: "https://3-oy-1-figmaa.vercel.app",
-        vercel: "",  
-    },
-    {
-      title: "figma-3",
-      github: "https://github.com/nilushaa/figma-3",
-      vercel: "https://figma-3-livid.vercel.app"
-    },
-  ];
-  
+  {
+    title: "nilushaa/3-oy-1-figmaa",
+    github: "https://3-oy-1-figmaa.vercel.app",
+    vercel: "",
+  },
+  {
+    title: "figma-3",
+    github: "https://github.com/nilushaa/figma-3",
+    vercel: "https://figma-3-livid.vercel.app",
+  },
+];
